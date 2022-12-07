00b3ff99fa54b27e14ce56675ed35e2690cf34b7
EcaCosca <enriquecoscarelli@gmail.com>
2022-12-07 11:22:23 +0100

    Set up

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const session = require('express-session');
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send("WELCOME!")
+})
+
+app.listen(port, ()=>{console.log(`http://localhost:${port}/`)})
